14061aab351922e34b105ebc934fed14eed715d5
Fahad <fahedihssan@gmail.com>
2025-02-05 17:50:34 -0500

    Final work

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const socketIo = require('socket.io');
 const path = require('path');
 
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth'); 
+const Message = require('./models/Message');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,17 +17,21 @@ const io = socketIo(server);
 app.use(cors());
 app.use(express.json());
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
-// Serve chat page at '/chat'
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/room-selection', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'room-selection.html'));
+});
+
 app.get('/chat', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'chat.html'));
 });
 
-
-// Authentication routes
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
@@ -36,36 +41,57 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Socket.IO setup
+// Socket.IO setup for chat rooms
 io.on('connection', (socket) => {
     console.log('A user connected.');
+    let username;
 
-    // Join Room
-    socket.on('joinRoom', (room) => {
+    
+    socket.on('joinRoom', ({ room, user }) => {
+        username = user;  
         socket.join(room);
-        console.log(`User joined room: ${room}`);
+        console.log(`${username} joined room: ${room}`);
+
+        // Load previous messages from MongoDB and send to the client
+        Message.find({ room })
+            .sort({ date_sent: 1 })
+            .then(messages => {
+                socket.emit('previousMessages', messages);
+            })
+            .catch(err => console.error('Error retrieving messages:', err));
+
+        io.to(room).emit('message', { user: 'admin', message: `${username} has joined the chat!` });
     });
 
-    // Leave Room
-    socket.on('leaveRoom', (room) => {
-        socket.leave(room);
-        console.log(`User left room: ${room}`);
+    socket.on('leaveRoom', (data) => {
+        socket.leave(data.room);
+        console.log(`${data.user} left room: ${data.room}`);
+        io.to(data.room).emit('message', { user: 'admin', message: `${data.user} has left the chat.` });
     });
 
-    // Send Message
     socket.on('message', ({ room, user, message }) => {
-        io.to(room).emit('message', { user, message });
+        const newMessage = new Message({ from_user: user, room, message });
+        newMessage.save()
+            .then(() => {
+                io.to(room).emit('message', { user, message });
+            })
+            .catch(err => console.error('Error saving message:', err));
     });
 
-    // Typing Indicator
-    socket.on('typing', (room) => {
-        socket.to(room).emit('typing', 'A user is typing...');
+    
+    socket.on('typing', (data) => {
+        if (data.user !== '') {
+            socket.to(data.room).emit('typing', data);
+        } else {
+            socket.to(data.room).emit('typing', { user: '' });
+        }
     });
 
-    // Disconnect
     socket.on('disconnect', () => {
-        console.log('A user disconnected.');
+        console.log(`${username} disconnected.`);
     });
 });
 
-server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
